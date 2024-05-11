171ffd8f01dce7e3e62279a86f0453a7ab3fd79f
rafi h <abda_rafi@yahoo.com>
2024-05-11 15:02:19 +0800

    formatter

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import streamlit as st
 
-st.title('PentraGuide - Penetration Testing Assistant')
+st.title("PentraGuide - Penetration Testing Assistant")
 
 # Text input from the user (penetration tester)
 input_findings = st.text_area(
-    'Enter your findings:', 
-    height=300, 
-    placeholder="Example: found SQL injection in the search product API.\n```\nGET /api/products?query=1%27%20OR%201%20=%201-- HTTP/1.1\nHost: example.com\n...\n\nHTTP/1.1 200 OK\nContent-Type: application/json\n...\n{\"message\":\"Successfully fetched products!\",\"data\":[...]}\n```"
+    "Enter your findings:",
+    height=300,
+    placeholder='Example: found SQL injection in the search product API.\n```\nGET /api/products?query=1%27%20OR%201%20=%201-- HTTP/1.1\nHost: example.com\n...\n\nHTTP/1.1 200 OK\nContent-Type: application/json\n...\n{"message":"Successfully fetched products!","data":[...]}\n```',
 )
 
+
 def check_input_sufficiency(input_text):
-    required_elements = ['description', 'request', 'response']
+    required_elements = ["description", "request", "response"]
     if all(element in input_text.lower() for element in required_elements):
         return True
     return False
 
+
 from langchain.prompts import PromptTemplate
+
 # Setting up the prompt template for report generation
 report_template = PromptTemplate(
-    input_variables=['findings'],
-    template='''Format the following penetration test findings into a Markdown formatted report:
+    input_variables=["findings"],
+    template="""Format the following penetration test findings into a Markdown formatted report:
     **Vulnerability Title:** Extracted or inferred from findings
     **Severity:** To be inferred from findings
     **Description:**
@@ -30,12 +33,14 @@ report_template = PromptTemplate(
     **References:** [If applicable]
     **Remediation:**
     Suggest remediations based on common practices for the vulnerability type identified.
-    '''
+    """,
 )
 
 from langchain.memory import ConversationBufferMemory
+
 memory = ConversationBufferMemory()
-memory.chat_memory.add_user_message('''
+memory.chat_memory.add_user_message(
+    """
                                     Description: I found a SQL Injection Vulnerability on the search page.
                                     
                                     Request:
@@ -46,8 +51,10 @@ memory.chat_memory.add_user_message('''
                                     HTTP/1.1 200 OK
                                     Content-Type: application/json
                                     {"message":"Successfully fetched products!","data":[...]}
-                                    ''')
-memory.chat_memory.add_ai_message('''### Vulnerability Title:
+                                    """
+)
+memory.chat_memory.add_ai_message(
+    """### Vulnerability Title:
 SQL Injection Vulnerability in Product Search API
 
 ### Severity:
@@ -74,32 +81,43 @@ CWE-89
                                   
 ### Remediation:
 Use parameterized queries or prepared statements to prevent SQL injection.
-''')
+"""
+)
 
 from langchain_community.llms import Ollama
+
 model = Ollama(model="lily")
 
 from langchain.chains import LLMChain
+
 # Create an LLMChain to generate Markdown formatted reports, retaining conversation context
-report_chain = LLMChain(llm=model, prompt=report_template, verbose=True, output_key='markdown_report', memory=memory)
+report_chain = LLMChain(
+    llm=model,
+    prompt=report_template,
+    verbose=True,
+    output_key="markdown_report",
+    memory=memory,
+)
 
 if input_findings:
     if check_input_sufficiency(input_findings):
         markdown_report = report_chain.run(findings=input_findings)
         st.markdown(markdown_report, unsafe_allow_html=True)
     else:
-        st.error("Information is not sufficient. Please provide more information with this format: \n"
-                 "- Short description about the vulnerability,\n"
-                 "- HTTP request,\n"
-                 "- HTTP response.\n\n"
-                 "Example:\n"
-                 "```\n"
-                 "Found SQL injection in the search product API.\n"
-                 "GET /api/products?query=1%27%20OR%201%20=%201-- HTTP/1.1\n"
-                 "Host: example.com\n"
-                 "...\n\n"
-                 "HTTP/1.1 200 OK\n"
-                 "Content-Type: application/json\n"
-                 "...\n"
-                 "{\"message\":\"Successfully fetched products!\",\"data\":[...]}\n"
-                 "```")
+        st.error(
+            "Information is not sufficient. Please provide more information with this format: \n"
+            "- Short description about the vulnerability,\n"
+            "- HTTP request,\n"
+            "- HTTP response.\n\n"
+            "Example:\n"
+            "```\n"
+            "Found SQL injection in the search product API.\n"
+            "GET /api/products?query=1%27%20OR%201%20=%201-- HTTP/1.1\n"
+            "Host: example.com\n"
+            "...\n\n"
+            "HTTP/1.1 200 OK\n"
+            "Content-Type: application/json\n"
+            "...\n"
+            '{"message":"Successfully fetched products!","data":[...]}\n'
+            "```"
+        )
